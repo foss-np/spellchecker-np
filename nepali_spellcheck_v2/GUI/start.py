6752f8bc15b8fa058a4941a13a8000f5643ecc2a
akshaylb <akshay.ksth@gmail.com>
2013-06-08 16:06:14 +0545

    Added watcher functions and fixed layout

--- nepali_spellcheck_v2/GUI/start.py
+++ nepali_spellcheck_v2/GUI/start.py
@@ -3,18 +3,23 @@ import sys
 from PyQt4 import QtGui,QtCore
 from PyQt4.QtGui import QMessageBox, QApplication, QMainWindow, QFileDialog
 from PyQt4.QtCore import pyqtSlot
-from design import Ui_Notepad
+from design import Ui_Spellcheck
+import codecs
+import codecs
+from os.path import isfile
 
 class StartQt4(QtGui.QMainWindow):
     def __init__(self,parent=None):
         QtGui.QWidget.__init__(self,parent)
-        self.ui=Ui_Notepad()
+        self.ui=Ui_Spellcheck()
         self.ui.setupUi(self)
+        self.watcher=QtCore.QFileSystemWatcher(self)
         QtCore.QObject.connect(self.ui.actionOpen,QtCore.SIGNAL("triggered()"),self.file_open)
         QtCore.QObject.connect(self.ui.actionNew,QtCore.SIGNAL("triggered()"),self.file_new)        
         QtCore.QObject.connect(self.ui.actionSave,QtCore.SIGNAL("triggered()"),self.file_save)
-        QtCore.QObject.connect(self.ui.actionClose,QtCore.SIGNAL("triggered()"),QtGui.qApp.quit)
+        QtCore.QObject.connect(self.ui.actionClose,QtCore.SIGNAL("triggered()"),self.close)
         QtCore.QObject.connect(self.ui.textEdit,QtCore.SIGNAL("textChanged()"),self.save_enable)
+        QtCore.QObject.connect(self.watcher,QtCore.SIGNAL("fileChanged(const QString&)"),self.file_changed)
         self.ui.actionSave.setEnabled(False)
         self.filename=""
 
@@ -32,13 +37,24 @@ class StartQt4(QtGui.QMainWindow):
         self.filename=QtGui.QFileDialog.getOpenFileName(self, "Open file", ".")
         from os.path import isfile
         if isfile(self.filename):
+            if self.filename:
+                self.watcher.removePath(self.filename)
             import codecs
             text=codecs.open(self.filename,'r','utf-8').read()
             self.ui.textEdit.setPlainText(text)
             self.ui.actionSave.setEnabled(False)
+            self.watcher.addPath(self.filename)
     
+    def file_changed(self,path):
+        res=QMessageBox.question(self, "%s - File has been changed" %self.objectName(),"The opened document has been modified by another program.\n"+"Do you want to reload the file?",QMessageBox.Yes|QMessageBox.No|(QMessageBox.Save if self.button.actionSave.isEnabled() else 0),QMessageBox.Yes)
+        if res == QMessageBox.Yes:
+            self.file_open(self.filename)
+        elif res == QMessageBox.Save:
+            self.file_save()
+            
     def file_save(self):
         from os.path import isfile
+        self.watcher.removePath(self.filename)
         if not isfile(self.filename):
             self.filename = QtGui.QFileDialog.getSaveFileName(self, 'Save File', '.txt')
         import codecs
@@ -46,6 +62,7 @@ class StartQt4(QtGui.QMainWindow):
         savetext.write(unicode(self.ui.textEdit.toPlainText()))
         savetext.close()
         self.ui.actionSave.setEnabled(False)
+        self.watcher.addPath(self.filename)
 
     def savecheck_dialog(self):    
         if not self.ui.actionSave.isEnabled():
@@ -55,6 +72,12 @@ class StartQt4(QtGui.QMainWindow):
         if res==QMessageBox.Save:
             self.file_save()
         return res==QMessageBox.Cancel
+        
+    def closeEvent(self,event):
+        if self.savecheck_dialog():
+            event.ignore()
+        else:
+            event.accept()
         
 if __name__=="__main__":
     app=QtGui.QApplication(sys.argv)
